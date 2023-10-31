bed07820b2da71fbf74237c6722215f66da236dc
Olivier Sené <sene.olivier.pro@gmail.com>
2023-10-31 09:57:49 +0100

    Mise en place du style du site grâce à Semantic UI

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
-import logo from '../../assets/logo.svg';
+import { Image } from 'semantic-ui-react';
 import Message from '../Message/Message';
 import ReposResults from '../ReposResults/ReposResults';
+import logoGithub from '../../assets/images/logo-github.png';
 import SearchBar from '../SearchBar/SearchBar';
 
 import './App.scss';
@@ -8,7 +9,9 @@ import './App.scss';
 function App() {
   return (
     <div className="app">
-      <header>Mon image</header>
+      <header className="app__header">
+        <Image src={logoGithub} alt="logo github" centered size="small" />
+      </header>
       <SearchBar />
       <Message />
       <ReposResults />
