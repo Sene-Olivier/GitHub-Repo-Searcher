8c2d65c385f12b1d0752dbd7b0c1ccad9d604e09
Olivier Sené <sene.olivier.pro@gmail.com>
2023-10-31 11:07:48 +0100

    Import des données statiques fournies dans le dossier data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,8 @@ import ReposResults from '../ReposResults/ReposResults';
 import logoGithub from '../../assets/images/logo-github.png';
 import SearchBar from '../SearchBar/SearchBar';
 
+import reposData from '../../data/repos';
+
 import './App.scss';
 
 function App() {
@@ -13,8 +15,8 @@ function App() {
         <Image src={logoGithub} alt="logo github" centered size="small" />
       </header>
       <SearchBar />
-      <Message message="La recherche a donné 115999 résultats" />
-      <ReposResults />
+      <Message message="La recherche a donnée 1159901 résultats" />
+      <ReposResults repos={reposData.items} />
     </div>
   );
 }
